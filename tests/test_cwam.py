4566f5a64e14a0ae9b0d07a325db1463113a9d73
Quentin Rousseau <quentin@instacart.com>
2018-05-17 16:33:50 -0700

    Fix tests

--- tests/test_cwam.py
+++ tests/test_cwam.py
@@ -28,9 +28,6 @@ class TestCwam(unittest.TestCase):
 
     def test_command_line_interface(self):
         runner = CliRunner()
-        result = runner.invoke(cli.main)
-        assert result.exit_code == 0
-        assert 'cwam.cli.main' in result.output
         help_result = runner.invoke(cli.main, ['--help'])
         assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        assert '-h, --help                      Show this message and exit.' in help_result.output  # noqa E501
